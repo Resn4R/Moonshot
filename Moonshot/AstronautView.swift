16625383c7f58cb61c41c8a0b18f0d3e0bb5fed4
Resn4R <purview-joust.0x@icloud.com>
2023-09-12 12:46:48 +0100

    moonshot app v1 100DoS_p8

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,34 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Vito Borghi on 11/09/2023.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView{
+            VStack{
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    static var previews: some View {
+        AstronautView(astronaut: astronauts["armstrong"]!)
+    }
+}
